62f2c98ace0ba07c3cc83adf07ce269a27faf2e0
eakhlak-khan <eakhlakk@gmail.com>
2026-09-21 14:04:20 +0530

    Update ifc.py

--- ifc.py
+++ ifc.py
@@ -9,7 +9,8 @@ import time
 app = Flask(__name__)
 CORS(app)
 
-API_KEY = "Api key paste area"
+API_KEY = 
+os.getenv("Api key paste area")
 
 WS_URL = (
     "wss://ws.twelvedata.com/v1/quotes/price"
